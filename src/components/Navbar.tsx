36ed3784cd0694e6464789270cde2f049e574f53
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-03-12 13:31:34 -0300

    Create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import * as S from "./styles";
+
+export const Navbar: React.FC = () => {
+  return (
+    <S.Navbar>
+      <ul>
+        <li>
+          <a href=".">Sobre</a>
+        </li>
+        <li>
+          <a href=".">Tecnologias</a>
+        </li>
+        <li>
+          <a href=".">Projetos</a>
+        </li>
+        <li>
+          <a href=".">Contato</a>
+        </li>
+      </ul>
+    </S.Navbar>
+  );
+};
