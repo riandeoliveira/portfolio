7d8ba5e66441ae6460deeb24470f4ec47d24eecd
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-03-12 13:31:03 -0300

    Create header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+import { Navbar } from "./Navbar";
+
+export const Header: React.FC = () => {
+  return (
+    <S.Header>
+      <Navbar />
+    </S.Header>
+  );
+};
