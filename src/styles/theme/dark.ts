bf7c09ed3295db54f343e19a07ba34ee7e861bf2
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-20 17:14:36 -0300

    ✨ feat: Add application themes

---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,9 @@
+const dark = {
+  background: "#121214",
+  blue_ribbon: "#006aeb",
+  other: "#0a0906",
+  storm_gray: "#737380",
+  white: "#ffffff",
+};
+
+export default dark;
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,9 @@
+const light = {
+  background: "#262626",
+  other: "#0a0906",
+  storm_gray: "#737380",
+  secondary_500: "#0052ff",
+  primary_50: "#f5f5f5",
+};
+
+export default light;
