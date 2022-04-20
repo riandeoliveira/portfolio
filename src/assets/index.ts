82b4d8f364b5505a5ef0a6b2da37e55988e65daa
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-20 17:12:00 -0300

    ✨ feat: Add new assets

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+import columnCard from "./images/3-column-preview-card-component.png";
+import avatar from "./images/avatar.jpg";
+import nftCard from "./images/nft-preview-card-component.png";
+import orderSummary from "./images/order-summary-component.png";
+import profileCard from "./images/profile-card-component.png";
+import qrCode from "./images/qr-code-component.png";
+import statsCard from "./images/stats-preview-card-component.png";
+import workspace from "./videos/workspace.mp4";
+import resume from "./images/resume.jpg";
+import sla from "./images/sla.jpg";
+import abc from "./images/abc.jpg";
+import space from "./images/space.jpg";
+
+const stackImages = [
+  columnCard,
+  nftCard,
+  orderSummary,
+  profileCard,
+  qrCode,
+  statsCard,
+];
+
+export { stackImages, avatar, workspace, resume, sla, abc, space };
