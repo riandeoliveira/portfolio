53198b0004c9897b7668e5ea60f1ecd775163e57
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-20 17:16:48 -0300

    ✨ feat: Create Resume component

---
+++ src/components/Resume/index.tsx
@@ -0,0 +1,28 @@
+import * as S from "./styles";
+import { TitleBox } from "../../styles/global/Globals";
+import { ParallaxBox } from "../../styles/global/Globals";
+import { resume } from "../../assets";
+
+const Resume = (): JSX.Element => {
+  return (
+    <S.Resume>
+      <TitleBox>
+        <h3>Meu Currículo</h3>
+        <h4>Mais informações sobre mim</h4>
+      </TitleBox>
+      <ParallaxBox image={resume}>
+        <S.ResumeBox>
+          <iframe
+            allow="autoplay"
+            height="480"
+            src="https://drive.google.com/file/d/1rWMGPnDs9gT283Iurq9B648n4gfcBMKt/preview"
+            title="Rian Oliveira's resume"
+            width="640"
+          ></iframe>
+        </S.ResumeBox>
+      </ParallaxBox>
+    </S.Resume>
+  );
+};
+
+export default Resume;
---
+++ src/components/Resume/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Resume = styled.section`
+  background-color: #121214;
+`;
+
+export const ResumeBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
