e71e23f147c816844c1174ac0b2aee2e1673e4e6
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-21 19:40:53 -0300

    📦 refactor: Update ```App.tsx```

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { ThemeProvider } from "styled-components";
-import dark from "./styles/theme/dark";
 import Home from "./components/Home";
 import About from "./components/About";
 import Stack from "./components/Stack";
@@ -9,14 +7,14 @@ import Footer from "./components/Footer";
 
 const App = (): JSX.Element => {
   return (
-    <ThemeProvider theme={dark}>
+    <>
       <Home />
       <About />
       <Stack />
       <Project />
       <Resume />
       <Footer />
-    </ThemeProvider>
+    </>
   );
 };
 
