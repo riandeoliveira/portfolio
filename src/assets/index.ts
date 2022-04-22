5d523548be10606f04ffafdd8df49d9396560aff
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-22 00:30:04 -0300

    📦 refactor: Update assets index

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,15 @@
-import columnCard from "./images/3-column-preview-card-component.png";
 import avatar from "./images/avatar.jpg";
+import columnCard from "./images/3-column-preview-card-component.png";
 import nftCard from "./images/nft-preview-card-component.png";
+import nightSky from "./images/night-sky.jpg";
+import notebook from "./images/notebook.jpg";
 import orderSummary from "./images/order-summary-component.png";
 import profileCard from "./images/profile-card-component.png";
 import qrCode from "./images/qr-code-component.png";
-import statsCard from "./images/stats-preview-card-component.png";
-import workspace from "./videos/workspace.mp4";
 import resume from "./images/resume.jpg";
-import sla from "./images/sla.jpg";
-import abc from "./images/abc.jpg";
-import space from "./images/space.jpg";
+import statsCard from "./images/stats-preview-card-component.png";
+import workspace from "./images/workspace.jpg";
+import code from "./videos/code.mp4";
 
 const stackImages = [
   columnCard,
@@ -20,4 +20,4 @@ const stackImages = [
   statsCard,
 ];
 
-export { stackImages, avatar, workspace, resume, sla, abc, space };
+export { stackImages, avatar, notebook, resume, nightSky, workspace, code };
