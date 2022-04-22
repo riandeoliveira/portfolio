2465c20e969768bc594acb59dca05e0bc06af36f
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-22 00:29:05 -0300

    ✨ feat: Create fonts file

---
+++ src/styles/fonts/index.ts
@@ -0,0 +1,11 @@
+import { Fonts } from "../../interfaces";
+
+const fonts: Fonts = {
+  primary: "Roboto",
+  secondary: "Oswald",
+  tertiary: "Bebas Neue",
+  quaternary: "Smooch Sans",
+  default: "sans-serif",
+};
+
+export default fonts;
