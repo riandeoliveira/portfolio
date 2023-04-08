7a487f913ffc905ef4a4fb6c48b0426a0906e00b
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-08 20:48:33 -0300

    feat: add avatar animations

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,10 +1,28 @@
-import AvatarImage from "../../../public/assets/images/avatar.svg";
+import { useState } from "react";
+import PrimaryAvatar from "../../../public/assets/images/primary-avatar.svg";
+import SecondaryAvatar from "../../../public/assets/images/secondary-avatar.svg";
 import styles from "./styles.module.scss";
 
 export const Avatar = (): JSX.Element => {
+  const [avatarAnimation, setAvatarAnimation] = useState<string>("");
+
+  const handleRotation = (): void => {
+    if (avatarAnimation === styles.primary_rotation) {
+      setAvatarAnimation(styles.secondary_rotation);
+    } else if (avatarAnimation === styles.secondary_rotation) {
+      setAvatarAnimation(styles.primary_rotation);
+    } else {
+      setAvatarAnimation(styles.primary_rotation);
+    }
+  };
+
   return (
-    <div className={styles.avatar_container}>
-      <AvatarImage className={styles.avatar} />
+    <div
+      className={`${styles.avatar_container} ${avatarAnimation}`}
+      onClick={handleRotation}
+    >
+      <SecondaryAvatar className={styles.secondary_avatar} />
+      <PrimaryAvatar className={styles.primary_avatar} />
       <div className={styles.avatar_background}></div>
     </div>
   );
