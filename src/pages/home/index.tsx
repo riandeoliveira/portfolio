07e4c542588344c7c117e80912a29c313e632d9d
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-14 19:50:00 -0300

    feat: update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,21 @@
 import { Header } from "components/Header";
 import { Navbar } from "components/Navbar";
 import type { NextPage } from "next";
+import { AboutMe } from "sections/AboutMe";
+import { HardSkills } from "sections/HardSkills";
+import { useSectionStore } from "store/useSectionStore";
 import styles from "./styles.module.scss";
 
 const Home: NextPage = (): JSX.Element => {
+  const sectionStore = useSectionStore();
+
   return (
     <div className={styles.page}>
       <div className={styles.page_container}>
         <Header />
         <Navbar />
+        {sectionStore.name === "about_me" && <AboutMe />}
+        {sectionStore.name === "hard_skills" && <HardSkills />}
       </div>
     </div>
   );
