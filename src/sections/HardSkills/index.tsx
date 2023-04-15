48090399984914bacdb842bf2e0bef9810a46e19
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-14 21:07:44 -0300

    refactor: convert svg code to a single svg file

--- src/sections/HardSkills/index.tsx
+++ src/sections/HardSkills/index.tsx
@@ -1,5 +1,6 @@
 import { Technology } from "components/Technology";
 import { technologyTreeLevels } from "data/technologyTreeLevels";
+import TechnologyTree from "../../../public/assets/images/technology-tree.svg";
 import styles from "./styles.module.scss";
 
 export const HardSkills = (): JSX.Element => {
@@ -13,151 +14,11 @@ export const HardSkills = (): JSX.Element => {
           ))}
         </div>
       </div>
-      <svg
-        className={styles.svg}
-        width="1125"
-        height="1125"
-        viewBox="0 0 200 1"
-        style={{ position: "absolute" }}
-      >
-        <path
-          d="M100 -85 Q 28 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 Q 171 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 L 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        />
-      </svg>
-      <svg
-        className={styles.svg}
-        width="1125"
-        height="1125"
-        viewBox="0 0 200 1"
-        style={{ position: "absolute", translate: "0 960px" }}
-      >
-        <path
-          d="M100 -85 Q 28 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 Q 171 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 L 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        />
-      </svg>
-      <svg
-        className={styles.svg}
-        width="1125"
-        height="1125"
-        viewBox="0 0 200 1"
-        style={{ position: "absolute", translate: "0 1920px" }}
-      >
-        <path
-          d="M100 -85 Q 28 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 Q 171 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 L 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        />
-      </svg>
-      <svg
-        className={styles.svg}
-        width="1125"
-        height="1125"
-        viewBox="0 0 200 1"
-        style={{ position: "absolute", translate: "0 2880px" }}
-      >
-        <path
-          d="M100 -85 Q 28 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 Q 171 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 L 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        />
-      </svg>
-      <svg
-        className={styles.svg}
-        width="1125"
-        height="1125"
-        viewBox="0 0 200 1"
-        style={{ position: "absolute", translate: "0 3840px" }}
-      >
-        <path
-          d="M100 -85 Q 28 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 Q 171 5 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        ></path>
-        <path
-          d="M100 -85 L 100 85"
-          stroke="#361973"
-          stroke-width="3"
-          fill="none"
-          className={styles.sla}
-        />
-      </svg>
+      <TechnologyTree className={styles.tree_1} />
+      <TechnologyTree className={styles.tree_2} />
+      <TechnologyTree className={styles.tree_3} />
+      <TechnologyTree className={styles.tree_4} />
+      <TechnologyTree className={styles.tree_5} />
     </section>
   );
 };
