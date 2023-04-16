df95b9d624eb6aa08f165ef6dc3eedd9959a55e0
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-16 11:17:50 -0300

    feat: improve Technology component

---
+++ src/components/TechnologyArea/index.tsx
@@ -0,0 +1,45 @@
+import { TechnologyModal } from "components/TechnologyModal";
+import { useModalStore } from "store/useModalStore";
+import { useTechnologyStore } from "store/useTechnologyStore";
+import { TechnologyType } from "types";
+import { State } from "types/state";
+import styles from "./styles.module.scss";
+
+type TechnologyProps = {
+  technologies: TechnologyType[];
+};
+
+export const TechnologyArea = ({
+  technologies,
+}: TechnologyProps): JSX.Element => {
+  const modalStore: State.Modal = useModalStore();
+  const technologyStore: State.Technology = useTechnologyStore();
+
+  return (
+    <div className={styles.technology_area}>
+      {technologies.map((technology, index) => {
+        const { icon: Icon, title } = technology;
+
+        return (
+          <>
+            <div className={styles.technology_container} key={index}>
+              <div
+                className={styles.technology}
+                onClick={() => {
+                  modalStore.open("technology");
+                  technologyStore.setTechnology(technology);
+                }}
+              >
+                <div className={styles.icon}>
+                  <Icon />
+                </div>
+                <span>{title}</span>
+              </div>
+            </div>
+            <TechnologyModal />
+          </>
+        );
+      })}
+    </div>
+  );
+};
