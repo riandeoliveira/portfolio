ca38bf206aa67fba6f2085a6c050ca57368a1fe9
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-25 00:18:19 -0300

    feat: remove external link inside SpotifyCurrentTrack component

--- src/components/SpotifyCurrentTrack/index.tsx
+++ src/components/SpotifyCurrentTrack/index.tsx
@@ -10,21 +10,21 @@ export const SpotifyCurrentTrack = ({
   userId,
   barColor,
 }: SpotifyCurrentTrackProps): JSX.Element => {
+  // TODO: Adicionar rolagem vertical contendo três seções:
+  // 1. Ouvindo agora no Spotify
+  // 2. Rádio Lo-fi
+  // 3. Rádio Synthwave
+
   return (
     <div className={styles.container}>
       <div className={styles.title_container}>
         <span className={styles.title}>Ouvindo agora no</span>
         <SiSpotify size={24} fill="#1db954" />
       </div>
-      <a
-        href="https://github.com/kittinan/spotify-github-profile"
-        className={styles.spotify_link}
-      >
-        <img
-          src={`https://spotify-github-profile.vercel.app/api/view?uid=${userId}&cover_image=true&theme=novatorem&show_offline=false&background_color=ffffff&interchange=false&bar_color=${barColor}&bar_color_cover=false`}
-          alt="spotify-github-profile"
-        />
-      </a>
+      <img
+        src={`https://spotify-github-profile.vercel.app/api/view?uid=${userId}&cover_image=true&theme=novatorem&show_offline=false&background_color=ffffff&interchange=true&bar_color=${barColor}&bar_color_cover=false`}
+        alt="spotify-github-profile"
+      />
     </div>
   );
 };
