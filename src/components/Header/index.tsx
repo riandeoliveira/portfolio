2d6539d5a6eb25b5926d321174ef4cb078d70137
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-04-25 00:16:08 -0300

    feat: include radios in Header with swiper and react-player

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import { Author } from "components/Author";
 import { Avatar } from "components/Avatar";
 import { SocialMedia } from "components/SocialMedia";
 import { SpotifyCurrentTrack } from "components/SpotifyCurrentTrack";
+import { YouTubeIntegratedRadio } from "components/YouTubeIntegratedRadio";
+import dynamic from "next/dynamic";
 import {
   SiGithub,
   SiGmail,
@@ -11,9 +13,17 @@ import {
   SiTwitter,
   SiWhatsapp,
 } from "react-icons/si";
+import { usePlayerStore } from "store/usePlayerStore";
+import { Swiper, SwiperSlide } from "swiper/react";
+import lofiRadioThumbnail from "../../../public/assets/images/lofi-radio-thumbnail.svg";
+import synthwaveRadioThumbnail from "../../../public/assets/images/synthwave-radio-thumbnail.svg";
 import styles from "./styles.module.scss";
 
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+
 export const Header = (): JSX.Element => {
+  const playerStore = usePlayerStore();
+
   return (
     <header className={styles.header}>
       <div className={styles.header_container}>
@@ -49,7 +59,52 @@ export const Header = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <SpotifyCurrentTrack userId="riandias2016" barColor="ef2ef2" />
+      <Swiper
+        mousewheel
+        direction="vertical"
+        loop
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        className={styles.slider}
+      >
+        <SwiperSlide>
+          <SpotifyCurrentTrack userId="riandias2016" barColor="ef2ef2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <YouTubeIntegratedRadio
+            title="Rádio Lo-Fi 🍃"
+            description="beats to relax/study to"
+            thumbnail={lofiRadioThumbnail}
+            playing={playerStore.lofiRadio.playing}
+            onPlay={playerStore.playLofiRadio}
+            onPause={playerStore.pauseLofiRadio}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <YouTubeIntegratedRadio
+            title="Rádio Synthwave 💫"
+            description="beats to chill/game to"
+            thumbnail={synthwaveRadioThumbnail}
+            playing={playerStore.synthwaveRadio.playing}
+            onPlay={playerStore.playSynthwaveRadio}
+            onPause={playerStore.pauseSynthwaveRadio}
+          />
+        </SwiperSlide>
+      </Swiper>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=jfKfPfyJRdk"
+        playing={playerStore.lofiRadio.playing}
+        width={300}
+        height={128}
+        className={styles.player}
+      />
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=MVPTGNGiI-4"
+        playing={playerStore.synthwaveRadio.playing}
+        width={300}
+        height={128}
+        className={styles.player}
+      />
     </header>
   );
 };
