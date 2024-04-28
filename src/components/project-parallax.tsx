6a7eb2868a2e59444e2a280fbce42c79de899eb8
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-04-28 00:52:16 -0300

    fix: fix text colors

--- src/components/project-parallax.tsx
+++ src/components/project-parallax.tsx
@@ -40,10 +40,10 @@ export const ProjectParallax = observer((): ReactElement => {
       className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
-        <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
+        <h1 className="text-2xl md:text-7xl font-bold text-zinc-100">
           Conheça mais <br /> sobre meu trabalho
         </h1>
-        <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+        <p className="max-w-2xl text-base md:text-xl mt-8 text-zinc-50">
           Ao longo da minha jornada, já desenvolvi uma variedade de produtos de alta qualidade
           utilizando as tecnologias mais recentes e requisitadas do mercado.
         </p>
