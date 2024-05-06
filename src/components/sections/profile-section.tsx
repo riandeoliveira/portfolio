f97e4ae60924998399fc75302bbb046a7ce991d8
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-05-06 17:00:38 -0300

    feat: improve avatar easter egg

--- src/components/sections/profile-section.tsx
+++ src/components/sections/profile-section.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Link } from "../form/link";
 import { HighlightBackground } from "../highlight-background";
 import { HighlightText } from "../highlight-text";
+import { Image } from "../image";
 import { NeonBackground } from "../neon-background";
 import { SocialMediasArea } from "../social-medias-area";
 import { Tooltip } from "../tooltip";
@@ -45,8 +46,14 @@ export const ProfileSection = observer((): ReactElement => {
           >
             <NeonBackground className="rounded-full p-0 w-64 h-64">
               <div
-                className={`bg-zinc-900 flex items-center justify-center w-[248px] h-[248px] rounded-full absolute translate-x-1 translate-y-1 ${isRotating === true ? "animate-toggleSecondary" : ""} ${isRotating === false ? "animate-togglePrimary" : ""}`}
+                className={`bg-zinc-900 gap-2 flex items-center justify-center w-[248px] h-[248px] rounded-full absolute translate-x-1 translate-y-1 flex-col ${isRotating === true ? "animate-toggleSecondary" : ""} ${isRotating === false ? "animate-togglePrimary" : ""}`}
               >
+                <span
+                  style={{ transform: "rotateY(180deg)" }}
+                  className="block bg-clip-text text-sm hover:animate-pulse font-semibold text-transparent bg-gradient-to-b from-indigo-500 to-purple-500"
+                >
+                  Você encontrou um Easter Egg!
+                </span>
                 <a
                   href="https://youtu.be/uHgt8giw1LY?si=MovtNeUKU20Qt6FQ"
                   target="_blank"
@@ -58,11 +65,12 @@ export const ProfileSection = observer((): ReactElement => {
                   <div className="h-px bg-gradient-to-b from-indigo-500 to-purple-500" />
                 </a>
               </div>
-              <img
+              <Image
                 src={image.avatar}
                 alt="Imagem de perfil"
                 width={248}
                 height={248}
+                skeletonClassName="w-[248px] h-[248px]"
                 className="rounded-full absolute translate-x-1 translate-y-1 z-10"
               />
             </NeonBackground>
