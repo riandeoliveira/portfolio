c051bd5599d1daddcdb75d2c68edfc6ed365c104
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-05-08 20:07:07 -0300

    update header nav link in mobile

--- src/layouts/header/header-nav-link.tsx
+++ src/layouts/header/header-nav-link.tsx
@@ -11,12 +11,16 @@ type HeaderNavLinkProps = {
 export const HeaderNavLink = ({ children, icon: Icon, to }: HeaderNavLinkProps): ReactElement => {
   return (
     <li className="flex-1 text-center flex justify-center">
-      <a href={to} className="font-semibold flex flex-col items-center gap-1 group" aria-label={children}>
+      <a
+        href={to}
+        className="font-semibold flex flex-col items-center gap-1 group"
+        aria-label={children}
+      >
         <Icon size={24} className="fill-purple-500 hidden tablet-s:block" />
         <span className="bg-clip-text text-transparent bg-gradient-to-b from-indigo-500 to-purple-500 tablet-s:hidden">
           {children}
         </span>
-        <NeonBackground className="w-full h-0.5 opacity-0 group-hover:opacity-100 transition-opacity tablet-s:hidden" />
+        <NeonBackground className="w-full h-0.5 p-0 opacity-0 group-hover:opacity-100 transition-opacity tablet-s:hidden" />
       </a>
     </li>
   );
