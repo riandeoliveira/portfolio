c531b0eff05b08f7239915b769c668cbe54ba4f9
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2024-11-11 10:12:59 -0300

    feat: update resume

--- src/components/resume-download-link.tsx
+++ src/components/resume-download-link.tsx
@@ -4,7 +4,7 @@ import type { ReactElement } from "react";
 
 export const ResumeDownloadLink = (): ReactElement => {
   return (
-    <Field.Link href="/CV_Rian_Dias_de_Oliveira.pdf" download className="gap-2 group/link">
+    <Field.Link href="/Rian_Dias_de_Oliveira.pdf" download className="gap-2 group/link">
       Baixar Currículo
       <Icon.HiDownload size={20} className="transition-all group-hover/link:animate-bounce" />
     </Field.Link>
