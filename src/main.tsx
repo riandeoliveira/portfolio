2db631602350d171b49795656b5fc1a92b6fe8f3
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2025-03-08 13:16:41 -0300

    feat: add dynamic lang to head tags

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "@/i18n";
+import { HelmetProvider } from "react-helmet-async";
 
 const rootElement = document.querySelector("#root") as Element;
 
 ReactDOM.createRoot(rootElement!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>,
 );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { LanguageSwitcher } from "@/components/language-switcher";
 import { OuterSpaceBackground } from "@/components/outer-space-background";
 import { SwitchModeButton } from "@/components/toggle-mode-button";
+import { useI18n } from "@/hooks/use-i18n";
 import { AboutSection } from "@/layouts/about/about-section";
 import { AchievementsSection } from "@/layouts/achievements/achievements-section";
 import { ContactSection } from "@/layouts/contact/contact-section";
@@ -13,10 +14,17 @@ import { SkillsSection } from "@/layouts/skills/skills-section";
 import { localStorageStore } from "@/stores/local-storage-store";
 import { observer } from "mobx-react-lite";
 import type { ReactElement } from "react";
+import { Helmet } from "react-helmet-async";
 
 export const Home = observer((): ReactElement => {
+  const { language, t } = useI18n();
+
   return (
     <>
+      <Helmet>
+        <html lang={language} />
+        <title>{t("home_page_head_title")}</title>
+      </Helmet>
       {localStorageStore.isQualityMode && <OuterSpaceBackground />}
       <HeaderArea />
       <main className="flex flex-col gap-24">
