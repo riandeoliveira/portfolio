f92029c1242dea0b0729213e1b24024327170585
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2025-03-17 23:55:28 -0300

    fix: fix import declarations error

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -1,10 +1,5 @@
 import type { ReactElement } from "react";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 interface RouteProps {
   page: () => ReactElement;
