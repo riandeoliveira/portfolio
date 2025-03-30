41fff029f9925241815ed329d4ed909a5c6b05bb
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2025-03-30 10:01:04 -0300

    refact: add missing strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import "@/i18n";
 import "@/main.css";
 import { router } from "@/routes";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 const rootElement = document.querySelector("#root") as Element;
 
-ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+createRoot(rootElement).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
